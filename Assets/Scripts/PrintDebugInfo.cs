af09bd6c31e80d27891bf62d0fb70869bbd3cd15
Brush <569171308@qq.com>
2015-11-05 02:01:00 +0800

    Fix Conflicts
    解决Debug信息冲突。

--- Assets/Scripts/PrintDebugInfo.cs
+++ Assets/Scripts/PrintDebugInfo.cs
@@ -6,7 +6,7 @@ public class PrintDebugInfo : MonoBehaviour
 	void Awake() 
     {
         Debug.Log("PrintDebugInfo is Runing!");
-	
+        Debug.Log("PrintDebugInfo Awake is Runing!");
 	}
 		
 	void Start() 
