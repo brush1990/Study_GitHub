4d4fdf1d415eb3a6c44de633aa5d63d4091a776a
Brush <569171308@qq.com>
2015-11-05 01:53:30 +0800

    Upload Files
    上传一个启动场景与代码文件

---
+++ Assets/Scripts/PrintDebugInfo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintDebugInfo : MonoBehaviour 
+{	
+	void Awake() 
+    {
+        Debug.Log("PrintDebugInfo is Runing!");
+	
+	}
+		
+	void Start() 
+    {
+        GameObject go = new GameObject();
+        go.name = "GameRoot";        
+	}
+}
