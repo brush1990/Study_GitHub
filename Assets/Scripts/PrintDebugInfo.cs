91d9ff5d5cd1dbe75aa12ba5a0e4821c68006337
Brush <569171308@qq.com>
2015-11-05 02:12:26 +0800

    Revert "Upload Files"
    This reverts commit 4d4fdf1d415eb3a6c44de633aa5d63d4091a776a.
    
    Conflicts:
    	Assets/Scripts/PrintDebugInfo.cs

--- Assets/Scripts/PrintDebugInfo.cs
+++ Assets/Scripts/PrintDebugInfo.cs
@@ -6,7 +6,6 @@ public class PrintDebugInfo : MonoBehaviour
 	void Awake() 
     {
         Debug.Log("PrintDebugInfo is Runing!");
-        Debug.Log("PrintDebugInfo Awake is Runing!");
 	}
 		
 	void Start() 
